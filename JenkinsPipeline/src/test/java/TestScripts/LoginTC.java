cfe3092bd1a7cb7d04342847b2eb6873adf7a8f4
sahil.kak <sahilkak007@gmail.com>
2019-06-30 01:33:14 +0530

    new changes

--- JenkinsPipeline/src/test/java/TestScripts/LoginTC.java
+++ JenkinsPipeline/src/test/java/TestScripts/LoginTC.java
@@ -7,5 +7,12 @@ public class LoginTC {
   public void loginTC() 
   {
 	  System.out.println("login is  successful");
+	
+  }
+  
+  @Test
+  public void logout() 
+  {
+	  System.out.println("logout is  successful");
   }
 }
