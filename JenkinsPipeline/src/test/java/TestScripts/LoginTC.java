95ad860cfef9fe072858490c671d25d772641d3c
sahil.kak <sahilkak007@gmail.com>
2019-06-29 13:17:25 +0530

    Adding files

---
+++ JenkinsPipeline/src/test/java/TestScripts/LoginTC.java
@@ -0,0 +1,11 @@
+package TestScripts;
+
+import org.testng.annotations.Test;
+
+public class LoginTC {
+  @Test
+  public void loginTC() 
+  {
+	  System.out.println("login is  successful");
+  }
+}
---
+++ JenkinsPipeline/src/test/java/TestScripts/TC1.java
@@ -0,0 +1,16 @@
+package TestScripts;
+
+import org.testng.annotations.Test;
+
+public class TC1 {
+
+	
+	@Test
+	public void TC()
+	{
+		
+		
+		System.out.println("This is a sample test case");
+	}
+	
+}
